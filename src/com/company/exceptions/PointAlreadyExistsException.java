b4e74bd5820ec14d1481913493fb343f52ef9cc2
Hubert Gawczyński <42291424+hubegaw@users.noreply.github.com>
2022-04-11 14:36:41 +0200

    Delete src/com/company directory

--- src/com/company/exceptions/PointAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.company.exceptions;
-
-public class PointAlreadyExistsException extends Exception{
-    public PointAlreadyExistsException(String message) {
-        super(message);
-    }
-}
